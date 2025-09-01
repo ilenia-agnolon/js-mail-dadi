aca93a65aaae4587045943dd9515b144d41f8799
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-01 15:32:52 +0200

    MAIL: creato array ed inserito input

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ Si può fare? Certo che si basta ragionare un po’.
 Nota: 
 Non è necessario provvedere alla validazione delle email
 _______________________________________________________________________________________________*/
+
+//creo array
+const guests = [
+    "guest1@gmail.com",
+    "guest2@gmail.com",
+    "guest3@gmail.com",
+    "guest4@gmail.com",
+    "guest5@gmail.com",
+    "guest6@gmail.com",
+    "guest7@gmail.com",
+    "guest8@gmail.com",
+    "guest9@gmail.com",
+    "guest10@gmail.com",
+]
+
+
+//INPUT: chiedo all'utente la sua mail
+const mail = (prompt("inserisci la tua mail"));
+
+    console.log("mail inserita:", mail);
+    
