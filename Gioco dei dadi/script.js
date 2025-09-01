de512268196348cc15524a988e107d11ee3b16ee
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-01 17:04:24 +0200

    inserisco math floor per far generare un numero casuale e stampo in console il risultato

--- Gioco dei dadi/script.js
+++ Gioco dei dadi/script.js
@@ -8,12 +8,15 @@ Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 _______________________________________________________________________________________________*/
 
 // 1. Genero il numero casuale per il giocatore
+const playerNumber = Math.floor(Math.random() * 6) + 1;
 
 
 // 2. Genero il numero casuale per il computer
+const computerNumber = Math.floor(Math.random() * 6) + 1;
 
 
 // 3. Stampo entrambi i numeri in console
+console.log("Numero del giocatore:",playerNumber, "Numero del computer:", computerNumber);
 
 
 // 4. Confronto i due numeri
