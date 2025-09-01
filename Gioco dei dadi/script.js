97f70c6caff13e88fdcdd55842e1994b0f89bdd9
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-01 16:52:32 +0200

    DADI: inserito passaggi logici

--- Gioco dei dadi/script.js
+++ Gioco dei dadi/script.js
@@ -6,3 +6,14 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 _______________________________________________________________________________________________*/
+
+// 1. Genero il numero casuale per il giocatore
+
+
+// 2. Genero il numero casuale per il computer
+
+
+// 3. Stampo entrambi i numeri in console
+
+
+// 4. Confronto i due numeri
